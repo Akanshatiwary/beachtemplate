284f58674348bfd407ed866cc79d1f6d53da1ef2
akansha <akanshatiwary03@gmail.com>
2020-08-20 17:02:13 +0530

    Updates

---
+++ precache-manifest.8a4c24581e811c843d4ac06a4ee9c916.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59f2fdc2c1f11c212847b75c0949566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3073585b9bb22355ea3b",
+    "url": "/static/css/main.a8e9d971.chunk.css"
+  },
+  {
+    "revision": "1242f905f6a227eac762",
+    "url": "/static/js/2.1f1cc256.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1f1cc256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3073585b9bb22355ea3b",
+    "url": "/static/js/main.37735869.chunk.js"
+  },
+  {
+    "revision": "cacef008c4d99995776e",
+    "url": "/static/js/runtime-main.0b71a8a2.js"
+  },
+  {
+    "revision": "b6b9e59476ff5e7b543147dbb8c355c5",
+    "url": "/static/media/beaches.b6b9e594.jpg"
+  },
+  {
+    "revision": "90a8acddbd6649a23fafe3e01103582f",
+    "url": "/static/media/sea.90a8acdd.jpg"
+  }
+]);
